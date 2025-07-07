c02205dc5b233fda64c79e8706e69bec44f406eb
Dylan Tong <dylantong17@gmail.com>
2025-07-07 14:48:35 +1200

    Finish Start Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Head from "./head";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Head />
+      <div className="center-container">
+        <h1 className="game-title">Welcome to Blackjack</h1>
+        <p className="game-desc">Get ready to play!</p>
+
+        <button className="start-btn">START</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/head.tsx
@@ -0,0 +1,16 @@
+
+
+function Head() {
+  return (
+    <>
+      <title>Blackjack - Dylan</title>
+      <meta name="description" content="Blackjack - Dylan" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    </>
+  );
+}
+
+export default Head;
